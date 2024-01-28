8fc564f881733539717115cc7be30496f3d6d0bc
KiranAleMagar77 <135760825+KiranAleMagar77@users.noreply.github.com>
2024-01-27 20:06:00 -0500

    made some changes

---
+++ src/app/helper-files/content-interface.ts
@@ -0,0 +1,11 @@
+interface Content{
+    id: number;
+    title: string;
+    description: string;
+    creator: string;
+    imgUrl?: string;
+    type?: string;
+    tags?: string;
+    }
+
+export default Content;
